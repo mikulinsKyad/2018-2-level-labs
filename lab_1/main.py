1092761fa879e6ad9a08a14be4a63c08cf3648b2
Artyom Mikulinsky <43423404+mikulinsKyad@users.noreply.github.com>
2018-09-24 17:52:19 +0400

    Добавлены три функции
    На проверку

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,21 +4,58 @@ Labour work #1
 Count frequencies dictionary by the given arbitrary text
 """
 
+def calculate_frequences(text):
+    if text != str(text):
+        return {}
+    word = ""
+    d = {}
+    s_list = []
+    text = text.lower()
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    text += " "
+    for letter in text:
+        if letter in letters:
+            word += letter
+        elif letter == " ":
+            if word != "":
+                s_list.append(word)
+            word = ""
+        else:
+            if word != "":
+                s_list.append(word)
+            word = ""
+    for str_in_list in s_list:
+        d[str_in_list] = s_list.count(str_in_list)
+    return d
 
-def calculate_frequences() -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    return {}
+def filter_stop_words(frequencies, stop_words):
+    if frequencies == None:
+        return {}
+    stop_frequencies = frequencies.copy()
+    if stop_words == None:
+        return stop_frequencies
+    stop_lst = []
+    for key2 in stop_frequencies:
+        stop_lst.append(key2)
+    for key3 in stop_lst:
+        if key3 != str(key3):
+            del(stop_frequencies[key3])
+    for woord in stop_words:
+        if woord in stop_frequencies:
+            del(stop_frequencies[woord])
+    return stop_frequencies
 
-def filter_stop_words() -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    return {}
 
-def get_top_n() -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+def get_top_n(frequencies, top_n):
+    import operator
+    dict_func1 = frequencies.copy()
+    if dict_func1 == {} or top_n <= 0:
+        return ()
+    lst_dict = sorted(dict_func1.items(), key=operator.itemgetter(1), reverse=True)
+    lst_keys = []
+    for el in lst_dict:
+        lst_keys.append(el[0])
+    if top_n >= len(lst_keys):
+        return tuple(lst_keys)
+    else:
+        return tuple(lst_keys[:top_n])
